835afc63172d64420da8e6ee961ae1e3687f6a07
Medachii <noe2laurent@gmail.com>
2023-09-18 09:41:20 -0400

    oskur

---
+++ Newtonix/Newtonix.cpp
@@ -0,0 +1,12 @@
+﻿// Newtonix.cpp : définit le point d'entrée de l'application.
+//
+
+#include "Newtonix.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
