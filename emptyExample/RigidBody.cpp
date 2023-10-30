9af3f6859f08d6d2bda5e5ac42651ff941d57019
Thomas Petit <6poules@gmail.com>
2023-10-30 10:13:12 -0400

    [+] les corps rigides là

--- emptyExample/RigidBody.cpp
+++ emptyExample/RigidBody.cpp
@@ -1,1 +1,85 @@
 #include "RigidBody.h"
+
+
+RigidBody::RigidBody()
+{
+}
+
+RigidBody::~RigidBody()
+{
+}
+
+double RigidBody::getInverseMass()
+{
+	return this->inverseMass;
+}
+
+void RigidBody::setInverseMass(double mass)
+{
+	this->inverseMass = mass;
+}
+
+double RigidBody::getLinearDamping()
+{
+	return this->linearDamping;
+}
+
+void RigidBody::setLinearDamping(double damping)
+{
+	this->linearDamping = damping;
+}
+
+Vecteur3D RigidBody::getPosition()
+{
+	return this->position;
+}
+
+void RigidBody::setPosition(Vecteur3D pos)
+{
+	this->position = pos;
+}
+
+Vecteur3D RigidBody::getVelocity()
+{
+	return this->velocity;
+}
+
+void RigidBody::setVelocity(Vecteur3D vel)
+{
+	this->velocity = vel;
+}
+
+Quaternion RigidBody::getOrientation()
+{
+	return this->orientation;
+}
+
+void RigidBody::setOrientation(Quaternion q)
+{
+	this->orientation = q;
+}
+
+Vecteur3D RigidBody::getRotation()
+{
+	return this->rotation;
+}
+
+void RigidBody::setRotation(Vecteur3D rot)
+{
+	this->rotation = rot;
+}
+
+Matrix34 RigidBody::getTransformMatrix()
+{
+	return this->transformMatrix;
+}
+
+void RigidBody::setTransformMatrix(Matrix34 matrix)
+{
+	this->transformMatrix = matrix;
+}
+
+void RigidBody::integrate(double duration)
+{
+}
+
--- emptyExample/RigidBody.h
+++ emptyExample/RigidBody.h
@@ -17,7 +17,24 @@ private:
 	Matrix34 transformMatrix;
 
 public :
+	RigidBody();
+	~RigidBody();
 	void integrate(double duration);
+	double getInverseMass();
+	void setInverseMass(double mass);
+	double getLinearDamping();
+	void setLinearDamping(double damping);
+	Vecteur3D getPosition();
+	void setPosition(Vecteur3D pos);
+	Vecteur3D getVelocity();
+	void setVelocity(Vecteur3D vel);
+	Quaternion getOrientation();
+	void setOrientation(Quaternion q);
+	Vecteur3D getRotation();
+	void setRotation(Vecteur3D rot);
+	Matrix34 getTransformMatrix();
+	void setTransformMatrix(Matrix34 matrix);
+
 private:
 	void calculateDerivedData();
 };
